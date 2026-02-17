e27c8f9ff3334e29bd3ee5018e5697e34c496946
Quahr0nL <quahron@protonmail.com>
2026-02-16 19:18:08 -0500

    added custom crusor

--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -1,25 +1,23 @@
-# starts and stops timer for attributes  
+# starts and stops timer for attributes; custom cursor  
 extends CharacterBody2D
+
 @onready var _idle = $AnimatedSprite2D
 @onready var _health = $AnimatedSprite2D2
 var health = 100
 var hunger = 100
 var activity = 100
-
+var arrow = load("res://Images/cursor.png")
 var monster_atts = [health,hunger,activity]
 
 func _ready():
+	Input.set_custom_mouse_cursor(arrow)
 	$Timer.start()
 	$Timer.wait_time = health
-	
 	print("your time has started now")
-
-func life_change(health):
-	$health.value = health
 
 func _process(_delta):
 	$health.value = $Timer.time_left 
-	if $Timer.time_left <= 50:
+	if $Timer.time_left and hunger <= 50:
 		print("im not feeling too good")
 		_health.play("health") 
 		_idle.play("idle")
@@ -28,11 +26,9 @@ func _process(_delta):
 		print("Im prerfectly fine")
 		_idle.play("idle")
 		
-		
+
 func _on_timer_timeout():
-	print("TIMER ")
-	health -= 0.5
-	print (health)
+	print("TIMER done ")
 	$Timer.start()
 
 
