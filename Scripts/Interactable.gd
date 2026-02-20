e9f968266ef4e372ac957bc596c3a23e762ee828
Zack Baker <zbaker94@gmail.com>
2026-02-19 20:31:40 -0500

    all kinds of stuff, y'all were there. you know.

---
+++ Scripts/Interactable.gd
@@ -0,0 +1,20 @@
+@abstract
+
+extends Node
+
+class_name Interactable
+
+@abstract
+func _on_hover()
+
+@abstract
+func _on_hover_exit()
+
+@abstract
+func _on_click()
+
+@abstract
+func _on_drag()
+
+@abstract
+func _on_drop()
--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -8,11 +8,13 @@ var global_stats: GlobalStats
 
 func on_hunger_change(new_value: float) -> void:
 	if new_value == 0:
-		global_stats.set_stat_current_value("health", 50)
+		print("im starving: " + str(new_value))
 	elif new_value <= 25:
-		print("im not feeling too good")
+		print("im not feeling too good: " + str(new_value))
+	elif new_value <= 75:
+		print("I could eat: " + str(new_value))
 	else:
-		print("Im prerfectly fine")
+		print("Im perfectly fine: " + str(new_value))
 
 func on_stat_change(stat_name: String, new_value: float) -> void:
 		if stat_name == "hunger":
