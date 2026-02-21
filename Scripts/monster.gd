70c76f709f134d6061ea8e1f931b469b08963a6f
Zack Baker <zbaker94@gmail.com>
2026-02-21 00:49:37 -0500

    add sad animation when hungry

--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -1,15 +1,29 @@
 # starts and stops timer for attributes; custom cursor  
 extends Node 
 
+@export var sprite: AnimatedSprite3D
+
 var global_stats: GlobalStats
+
+func _play_idle():
+	sprite.animation = "idle"
+	sprite.play()
+	sprite.animation_finished.disconnect(_play_idle)
+	
+func _play_sad():
+	sprite.animation = "sad"
+	sprite.play()
+	sprite.animation_finished.connect(_play_idle)
 
 func on_hunger_change(new_value: float) -> void:
 	if new_value == 0:
 		print("im starving: " + str(new_value))
 	elif new_value <= 25:
 		print("im not feeling too good: " + str(new_value))
+		_play_sad()
 	elif new_value <= 75:
 		print("I could eat: " + str(new_value))
+		_play_sad()
 	else:
 		print("Im perfectly fine: " + str(new_value))
 
